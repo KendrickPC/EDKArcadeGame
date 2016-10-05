701d1e9e60eab245d0b97af82131f455e29d3119
KendrickPC <kennethpchang@gmail.com>
2016-10-05 12:22:19 +0800

    player can move, player reset function added, player boundaries added, and positioning has been tinkered with

--- js/app5.js
+++ js/app5.js
@@ -1,13 +1,15 @@
 // Enemies our player must avoid
-var Enemy = function(x, y) {
+var Enemy = function(x,y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
 };
+
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
@@ -16,32 +18,35 @@ Enemy.prototype.update = function(dt) {
     // all computers.
     this.x = this.x + 101 * dt;
 };
+
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(x, y) {
-    this.sprite = 'images/char-princess-girl.png';
+var Player = function(x,y) {
+    this.sprite = 'images/char-boy.png';
     this.x = x;
     this.y = y;
 };
 
-Player.prototype.handleInput = function(direction) {
+Player.prototype.handleInput = function(dir) {
 
-    if (direction === 'up') {
+    if (dir == 'up') {
         this.y = this.y - 80;
 
-    } else if (direction === 'down') {
+    } else if (dir == 'down') {
         this.y = this.y + 80;
 
-    } else if (direction === 'left') {
+    } else if (dir == 'left') {
         this.x = this.x - 101;
 
-    } else if (direction === 'right') {
+    } else if (dir == 'right') {
         this.x = this.x + 101;
+
     };
 
     if (this.x < 0 || this.x > 404) {
@@ -49,28 +54,34 @@ Player.prototype.handleInput = function(direction) {
     } else if (this.y < -25 || this.y > 404) {
         this.reset();
     };
-};
 
-Player.prototype.update = function() {
-    this.x = this.x;
-    this.y = this.y;
-};
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 Player.prototype.reset = function() {
     this.x = 202;
     this.y = 380;
 }
+
+Player.prototype.update = function() {
+    this.x = this.x;
+    this.y = this.y;
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-ladyBug1 = new Enemy(0, 225);
-ladyBug2 = new Enemy(-303, 140);
-ladyBug3 = new Enemy (-505, 60);
-allEnemies = [ladyBug1, ladyBug2, ladyBug3];
+enemy1 = new Enemy(0, 225);
+enemy2 = new Enemy(-303, 140);
+enemy3 = new Enemy (-505, 60);
+allEnemies = [enemy1, enemy2, enemy3];
+
 player = new Player(202, 380);
+
+
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
@@ -80,19 +91,6 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
+
     player.handleInput(allowedKeys[e.keyCode]);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
